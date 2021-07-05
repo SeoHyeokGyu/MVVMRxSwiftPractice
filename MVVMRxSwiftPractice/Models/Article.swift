f50a9378463e6e247a2b5c0e1429aa6368870886
SeoHyeokGyu <shk64099@gmail.com>
2021-07-05 15:31:41 +0900

    #Create Model

---
+++ MVVMRxSwiftPractice/Models/Article.swift
@@ -0,0 +1,24 @@
+//
+//  Article.swift
+//  MVVMRxSwiftPractice
+//
+//  Created by 서혁규 on 2021/07/05.
+//
+
+import Foundation
+//Codable : JSON 형식을 Swift 구조체 형식으로 컨버트할 수 있게 해준다.
+
+struct ArticleResponse:Codable {
+    let status:String
+    let totalResult:Int
+    let articles:[Article]
+}
+struct Article:Codable {
+    let author:String?
+    let title:String?
+    let descriptions:String?
+    let url:String?
+    let urlToImage:String?
+    let publishedAt:String?
+    
+}
