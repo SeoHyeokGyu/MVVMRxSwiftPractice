095e3997da6faf94344907898895c64ee638d9e0
SeoHyeokGyu <shk64099@gmail.com>
2021-07-05 15:23:49 +0900

    #Cocoa pod install pod 'Alamofire', '~> 5.2' pod 'RxSwift', '~> 5' pod 'RxCocoa', '~> 5' pod 'SDWebImage', '~> 5.0'

---
+++ MVVMRxSwiftPractice/Controllers/RootViewController.swift
@@ -0,0 +1,23 @@
+//
+//  RootViewController.swift
+//  MVVMRxSwiftPractice
+//
+//  Created by 서혁규 on 2021/07/05.
+//
+
+import UIKit
+
+class RootViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        configureUI()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    func configureUI() {
+        view.backgroundColor = .systemBackground
+    }
+
+}
---
+++ MVVMRxSwiftPractice/Coodinator.swift
@@ -0,0 +1,27 @@
+//
+//  Coodinator.swift
+//  MVVMRxSwiftPractice
+//
+//  Created by 서혁규 on 2021/07/05.
+//
+
+import UIKit
+
+class Coodinator{
+    let window:UIWindow
+    
+    init(window:UIWindow){
+        self.window = window
+    }
+    
+    func start() {
+        let rootViewController = RootViewController()
+        let navigationRootController = UINavigationController(rootViewController: rootViewController)
+       //지정
+        window.rootViewController = navigationRootController
+        // 화면에 렌더링
+        window.makeKeyAndVisible()
+        
+    }
+    
+}
